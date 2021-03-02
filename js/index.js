8dc080c26d455305fe8e05e360d23013516b5e4c
louise <louise.lelievre@ge.com>
2021-03-02 11:33:14 -0500

    js file creation

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// chargement dans une variable de la balise <div id="jsLoaded">. . .</div>
+var jsloaded=document.querySelector("#jsLoaded");
+
+// modif du style css de la balise
+jsloaded.style.backgroundColor="green";
+
+//modif du contenu textuel de la balise
+jsloaded.innerText="Le js est chargé";
